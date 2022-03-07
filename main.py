ef5cddd42ca8488efc7fbacfe6ad6974dc69b3db
bana#1337 <97836926+0jv@users.noreply.github.com>
2022-03-07 13:51:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ class Massban:
       self.headers = {"Authorization" : f"Bot {self.token}"}
       self.users = open("users.txt").read().splitlines()
 
-    def request_sender(self):
+    def request_sender(self, session):
       tasks = []
       for user in self.users:
-        tasks.append(asyncio.create_task(aiohttp.ClientSession(headers = self.headers).put(f"https://discord.com/api/v9/guilds/{self.guild}/bans/{user}")))
+        tasks.append(asyncio.create_task(session.put(f"https://discord.com/api/v9/guilds/{self.guild}/bans/{user}")))
       return tasks
 
     async def gather_tasks(self):
-      tasks = self.request_sender()
-      await asyncio.gather(*tasks)
+      async with aiohttp.ClientSession(headers = self.headers) as session:
+        tasks = self.request_sender(session)
+        await asyncio.gather(*tasks)
 
 if __name__ == "__main__":
   massban = Massban()
