e00d0f5c5e8e2e50750ad60da6cbe079d31e84d2
bana#0001 <97836926+0jv@users.noreply.github.com>
2022-03-18 02:52:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import aiohttp, asyncio
+import aiohttp, asyncio, random
 
 class Massban:
 
@@ -6,18 +6,29 @@ class Massban:
       self.token = input("Token -> ")
       self.guild = input("Guild -> ")
       self.headers = {"Authorization" : f"Bot {self.token}"}
-      self.users = open("users.txt").read().splitlines()
+      self.members = open("members.txt").read().splitlines()
+      self.status_codes = [200, 201, 204]
 
-    def request_sender(self, session):
-      tasks = []
-      for user in self.users:
-        tasks.append(asyncio.create_task(session.put(f"https://discord.com/api/v9/guilds/{self.guild}/bans/{user}")))
-      return tasks
+    async def request_sender(self, session, member):
+      async with session.put(f"https://discord.com/api/v{random.randint(8, 9)}/guilds/{self.guild}/bans/{member}", headers = self.headers) as r:
+        if r.status in self.status_codes:
+          print(f"[{r.status}] -> Banned Member {member}")
+        else:
+          print(f"[{r.status}] -> Error Banning Member {member}")
+        return await r.text()
+    
+    
+    async def fetch_all(self, session):
+        tasks = []
+        for member in self.members:
+            task = asyncio.create_task(self.request_sender(session, member))
+            tasks.append(task)
+        res = await asyncio.gather(*tasks)
+        return res
 
-    async def gather_tasks(self):
+    async def gather_tasks(self): 
       async with aiohttp.ClientSession(headers = self.headers) as session:
-        tasks = self.request_sender(session)
-        await asyncio.gather(*tasks)
+            await self.fetch_all(session)
 
 if __name__ == "__main__":
   massban = Massban()
